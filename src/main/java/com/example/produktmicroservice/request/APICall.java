e36ff8e2e0d7d67387d9e0d081635f0dc685cb32
Melisa <melcafidan@gmail.com>
2022-09-19 16:56:46 +0200

    send products

---
+++ src/main/java/com/example/produktmicroservice/request/APICall.java
@@ -0,0 +1,10 @@
+package com.example.produktmicroservice.request;
+
+public enum APICall {
+    GET_CARDS,
+    GET_DECKS,
+    GET_PRICE,
+    GET_CURRENCY,
+
+    CREATE_DECK,
+}
--- src/main/java/com/example/produktmicroservice/request/RequestHandler.java
+++ src/main/java/com/example/produktmicroservice/request/RequestHandler.java
@@ -1,8 +1,9 @@
 package com.example.produktmicroservice.request;
 
-import com.example.produktmicroservice.Service.PokemonService;
+import com.example.produktmicroservice.Service.ProductService;
 import com.google.gson.Gson;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -10,10 +11,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 public class RequestHandler {
 
     @Autowired
-    private PokemonService pokemonService;
+    private ProductService productService;
 
     @RabbitListener(queues = "${queue.product}")
-    public String getCards() {
-        return new Gson().toJson(pokemonService.getPokemonList());
+    public String handleRequest(Message message) {
+        APICall requestType = APICall.valueOf(message.getMessageProperties().getType());
+
+        switch (requestType) {
+            case GET_CARDS -> {
+                return new Gson().toJson(productService.getPokemonCardList());
+            }
+            case GET_DECKS -> {
+                String test = new Gson().toJson(productService.getPokemonCardDeckList());
+                return test;
+            }
+            default -> {
+                return "Error: Product Service Request Handler";
+            }
+        }
+
+
     }
 }
